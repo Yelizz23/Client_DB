d92c0fed078ab93690da6ed500b1738b6b970246
Eleonora <103573450+Yelizz23@users.noreply.github.com>
2022-07-14 09:04:57 +0200

    Functions description added

--- client_db.py
+++ client_db.py
@@ -9,7 +9,8 @@ with conn.cursor() as cur:
     DROP TABLE client
    ''')
 
-
+    
+#  Функция, создающая структуру БД (таблицы);
 def create_table():
     with conn.cursor() as cur:
         cur.execute('''
@@ -28,7 +29,8 @@ def create_table():
         );
         ''')
 
-
+        
+#  Функция, позволяющая добавить нового клиента;
 def add_client(name_client, surname_client, email, phone):
     with conn.cursor() as cur:
         cur.execute('''
@@ -39,7 +41,8 @@ def add_client(name_client, surname_client, email, phone):
         INSERT INTO phone_number(phone, client_id) VALUES (%s, %s);
         ''', (phone, res))
 
-
+        
+#  Функция, позволяющая добавить телефон для существующего клиента;
 def add_phone(name, phone):
     with conn.cursor() as cur:
         cur.execute('''
@@ -50,7 +53,8 @@ def add_phone(name, phone):
         INSERT INTO phone_number(phone, client_id) VALUES (%s, %s);
         ''', (phone, res))
 
-
+        
+#  Функция, позволяющая изменить данные о клиенте;
 def update_client(name, last_name, new_name, new_surname, new_email):
     with conn.cursor() as cur:
         cur.execute('''
@@ -61,7 +65,8 @@ def update_client(name, last_name, new_name, new_surname, new_email):
         UPDATE client SET first_name=%s, surname=%s, email=%s WHERE id=%s;
         ''', (new_name, new_surname, new_email, res))
 
-
+        
+#  Функция, позволяющая удалить телефон для существующего клиента;
 def delete_phone(name, last_name):
     with conn.cursor() as cur:
         cur.execute('''
@@ -72,7 +77,8 @@ def delete_phone(name, last_name):
         DELETE FROM phone_number WHERE id=%s;
         ''', (res,))
 
-
+        
+#  Функция, позволяющая удалить существующего клиента;
 def delete_client(name, last_name):
     with conn.cursor() as cur:
         cur.execute('''
@@ -87,7 +93,8 @@ def delete_client(name, last_name):
         DELETE FROM client WHERE first_name=%s AND surname=%s
         ''', (name, last_name))
 
-
+        
+#  Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону);
 def find_client(name, last_name, e_mail, phone_num):
     with conn.cursor() as cur:
         cur.execute('''
@@ -107,19 +114,23 @@ add_client('Melany', 'Cleo', 'cleo.melany@gmail.com', '+420721567544')
 add_client('Thomas', 'Cook', 'cook.th@hotmail.com', '+420602252787')
 add_client('Lesley', 'Ellers', 'miss.ellers@hotmail.com', '+420777187777')
 add_client('Martin', 'Freeman', 'marty.free@gmail.com', '+42060660352')
+
 add_phone('Smith', '+420602272666')
 add_phone('Smith', '+420777875355')
 add_phone('Smith', '+420606879456')
 add_phone('Cleo', '+420773183478')
 add_phone('Ellers', '+420773264355')
 add_phone('Freeman', '+420774555178')
+
 update_client('Melany', 'Cleo', 'Melany', 'Seeto', 'seeto.mel@gmail.com')
 update_client('Martin', 'Freeman', 'Martin', 'Freeman', 'martin.freeman@hotmail.com')
 update_client('Alison', 'Dunlop', 'Alison', 'Moore', 'moore.al@gmail.com')
+
 delete_phone('Audrey', 'Smith')
 delete_phone('Alison', 'Moore')
 delete_phone('Melany', 'Seeto')
 delete_client('Melany', 'Seeto')
+
 find_client('Audrey', 'Smith', 'smith.a@gmail.com', '+420777875355')
 
 conn.commit()
